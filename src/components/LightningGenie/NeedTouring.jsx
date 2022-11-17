7a5e48d064aa0ccb88c811c194b156f74142d486
ahsanbilalas <ahsan.bilal@arbisoft.com>
2022-11-17 14:56:40 +0500

    Fixed Ui for font geneie

--- src/components/LightningGenie/NeedTouring.jsx
+++ src/components/LightningGenie/NeedTouring.jsx
@@ -6,7 +6,7 @@ const NeedTouring = () => {
   return (
     <section className="bg-tp-purple">
       <div className="container md:max-w-960 mx-auto bg-primary font-eucliar text-center py-12 px-4">
-        <h1 className="xs:text-24 md:text-30 mb-3 xs:px-4 md:px-0  font-groteskSemiBold text-white font-semibold leading-8">
+        <h1 className="xs:text-24 md:text-30 mb-3 xs:px-4 md:px-0 font-groteskBold text-white font-semibold leading-8">
         Do I still need a Touring Plan if I use Genie+?
         </h1>
         <p className="xs:text-15 md:text-18 mb-4 font-normal">YES! Even with the potential to schedule your attractions, you’ll definitely want to use a Touring Plan to know the best time to visit each attraction and whether there’s a way to save paying for an Individual Lightning Lane. </p>
