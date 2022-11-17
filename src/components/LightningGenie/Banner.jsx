eca45f6469bdcecf2eb8a5752dc80bd0eb11760f
ahsanbilalas <ahsan.bilal@arbisoft.com>
2022-11-17 14:22:33 +0500

    Fixed difference section

--- src/components/LightningGenie/Banner.jsx
+++ src/components/LightningGenie/Banner.jsx
@@ -4,14 +4,15 @@ import mobileHand from "assets/images/mobileHand.png"
 const Banner = () => {
   return (
     <section className="bg-tp-bluish-300 text-white">
-      <div className="container mx-auto bg-primary text-center py-67 px-2">
-        <div className="flex justify-center">
-          <img className="shadow rounded-full max-w-full h-auto align-middle border-none" src={mobileHand} alt="mobileHand" width="134px" />
+      <div className="container mx-auto bg-primary text-center xs:py-9 md:py-67 px-2">
+        <div className="flex justify-center xs:mb-7 md:mb-11">
+          <img className="xs:w-[74px] md:w-[134px] shadow rounded-full max-w-full h-auto align-middle border-none" src={mobileHand} alt="mobileHand" />
         </div>
         <h1 className="text-36 md:text-50 xl:text-60 font-groteskBold font-semibold mb-3">
           Genie+ & Lightning Lane
         </h1>
         <div className='max-w-[640px] mx-auto'>
+          <p className="mb-3">Disney’s latest iteration of FASTPASS has multiple components that can help you get the most from your day. We’ll show you how making the right choices (with the use of a Touring Plan) can make it even better.</p>
           <p>
             Last updated: October 12, 2022
           </p>
--- src/components/LightningGenie/TimeSaving.jsx
+++ src/components/LightningGenie/TimeSaving.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 
 const Row = ({ isOdd, title, avgTime, highestCost, costPerHour }) => {
   return (
-    <div className={`flex py-9 leading-7 text-18  ${isOdd ? "bg-tp-gray-400" : ""}`}>
-      <div className="flex-1 flex flex-row font-bold items-center">
-        <img class="mb-3 mr-2 shadow rounded-full max-w-full h-auto align-middle border-none" src="https://via.placeholder.com/223x223.png" alt="" width="50px" height="50px" />
-        {title}
-      </div>
-      <div className="flex-1 flex justify-center items-center text-36 font-light font-eucliarLight">{avgTime}</div>
-      <div className="flex-1  flex justify-center items-center text-36 font-light font-eucliarLight">
-        <small className='text-18'>$</small>{highestCost}
-      </div>
-      <div className="flex-1  flex justify-center  items-center text-36 font-light font-eucliarLight">
-        <small className='text-18'>$</small>{costPerHour}
+    <div className={`${isOdd ? "bg-tp-gray-400" : ""}`}>
+      <div className="w-[1300px] mx-auto">
+        <div className={`flex py-9 leading-7 text-18`}>
+          <div className="flex-1 flex flex-row font-bold items-center">
+            <img class="mb-3 mr-2 shadow rounded-full max-w-full h-auto align-middle border-none" src="https://via.placeholder.com/223x223.png" alt="" width="50px" height="50px" />
+            {title}
+          </div>
+          <div className="flex-1 flex justify-center items-center text-36 font-light font-eucliarLight">{avgTime}</div>
+          <div className="flex-1  flex justify-center items-center text-36 font-light font-eucliarLight">
+            <small className='text-18'>$</small>{highestCost}
+          </div>
+          <div className="flex-1  flex justify-center  items-center text-36 font-light font-eucliarLight">
+            <small className='text-18'>$</small>{costPerHour}
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -28,40 +32,46 @@ const TimeSaving = () => {
             What Individual Lightning Lanes can I purchase and how much time could it save me?
           </h1>
         </div>
-        <section>
-          <header className="flex mb-6 font-semibold">
-            <div className="flex-1"></div>
-            <div className="flex-1">Average Time in Line Saved</div>
-            <div className="flex-1">Highest Cost</div>
-            <div className="flex-1">Cost Saved Per Hour</div>
-          </header>
-          <Row
-            isOdd={true}
-            image="https://via.placeholder.com/223x223.png"
-            title="Rise of the Resistance"
-            avgTime="91 minutes"
-            highestCost="15"
-            costPerHour="9.90"
-          />
+        <div>
+          <div className='table-header'>
+            <div className='w-[1300px] mx-auto'>
+              <div className="flex mb-6 font-semibold">
+                <div className="flex-1"></div>
+                <div className="flex-1">Average Time in Line Saved</div>
+                <div className="flex-1">Highest Cost</div>
+                <div className="flex-1">Cost Saved Per Hour</div>
+              </div>
+            </div>
+          </div>
+          <div className='table-body'>
+            <Row
+              isOdd={true}
+              image="https://via.placeholder.com/223x223.png"
+              title="Rise of the Resistance"
+              avgTime="91 minutes"
+              highestCost="15"
+              costPerHour="9.90"
+            />
 
-          <Row
-            isOdd={false}
-            image="https://via.placeholder.com/223x223.png"
-            title="Meet Mickey Mouse at Town Square Theater"
-            avgTime="91 minutes"
-            highestCost="15"
-            costPerHour="9.90"
-          />
+            <Row
+              isOdd={false}
+              image="https://via.placeholder.com/223x223.png"
+              title="Meet Mickey Mouse at Town Square Theater"
+              avgTime="91 minutes"
+              highestCost="15"
+              costPerHour="9.90"
+            />
 
-          <Row
-            isOdd={true}
-            image="https://via.placeholder.com/223x223.png"
-            title="Monster’s Inc. Laugh Floor"
-            avgTime="91 minutes"
-            highestCost="15"
-            costPerHour="9.90"
-          />
-        </section>
+            <Row
+              isOdd={true}
+              image="https://via.placeholder.com/223x223.png"
+              title="Monster’s Inc. Laugh Floor"
+              avgTime="91 minutes"
+              highestCost="15"
+              costPerHour="9.90"
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
