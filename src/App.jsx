c4523abc420f23a15936fee777635882cfed7507
ahsanbilalas <ahsan.bilal@arbisoft.com>
2022-12-04 14:18:33 +0500

    Implemented attraction card

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import './App.css';
 import HotelLanding from 'pages/HotelLanding';
 import LightningGenie from 'pages/LightningGenie';
 import ParkLanding from 'pages/ParkLanding';
+import Attraction from 'pages/Attraction';
 
 const App = () => {
   return (
     <>
       {false && <HotelLanding />}
       {false && <LightningGenie />}
-      {true && <ParkLanding />}
-      
+      {false && <ParkLanding />}
+      {true && <Attraction />}
     </>
   );
 }
---
+++ src/pages/Attraction/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import AttractionCard from 'components/Attraction';
+import Container from 'elements/Container';
+
+const Attraction = () => {
+  return (
+    <Container bg="bg-tp-purple" className="bg-white md:!max-w-[850px]" >
+      <div className='xs:max-w-[342px] md:max-w-[800px] mx-auto'>
+        <AttractionCard />
+        <AttractionCard />
+      </div>
+    </Container>
+  );
+};
+
+export default Attraction;
