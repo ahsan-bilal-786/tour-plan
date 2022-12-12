f0f1a11642cf0c6bfb4280a4294693de3d52c9db
ahsanbilalas <ahsan.bilal@arbisoft.com>
2022-12-12 23:34:17 +0500

    Implemented react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,41 @@
 import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import HotelLanding from 'pages/HotelLanding';
 import LightningGenie from 'pages/LightningGenie';
 import ParkLanding from 'pages/ParkLanding';
 import Attraction from 'pages/Attraction';
 import Search from 'pages/Search';
 
+const router = createBrowserRouter([
+  {
+    path: "/hotel_Landing",
+    element: <HotelLanding />,
+  },
+  {
+    path: "/lightning_genie",
+    element: <LightningGenie />,
+  },
+  {
+    path: "/park_landing",
+    element: <ParkLanding />,
+  },
+  {
+    path: "/attraction",
+    element: <Attraction />,
+  },
+  {
+    path: "/",
+    element: <Search />,
+  },
+]);
+
 const App = () => {
   return (
     <>
-      {false && <HotelLanding />}
-      {false && <LightningGenie />}
-      {false && <ParkLanding />}
-      {false && <Attraction />}
-      {true && <Search />}
-      
+      <RouterProvider router={router} />
     </>
   );
 }
