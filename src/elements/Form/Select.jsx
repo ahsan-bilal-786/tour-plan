cb3229b826ffc39fb56dd08a9d0c8f275cf7b543
Ahsan Bilal <engr.ahsan.bilal@gmail.com>
2023-02-04 23:36:09 +0500

    Implemented ticket calculator (#12)
    Co-authored-by: ahsanbilalas <ahsan.bilal@arbisoft.com>

--- src/elements/Form/Select.jsx
+++ src/elements/Form/Select.jsx
@@ -3,10 +3,10 @@ import map from "lodash/map";
 
 const Select = ({ label, options, className, ...rest }) => {
   return (
-    <div className={`flex flex-col xs:mb-5 md:mb-11 xs:w-full md:w-auto md:max-w-[500px] ${className}`}>
-      <label className='text-[13px] text-black font-medium uppercase'>{label}</label>
+    <div className={`flex flex-col xs:mb-5 md:mb-7 xs:w-full md:w-auto md:max-w-[500px] ${className}`}>
+      <label className='text-[13px] text-black font-eucliarMedium font-medium uppercase leading-8'>{label}</label>
       <select
-        className='border-[2px] border-tp-gray-300 py-4 px-4'
+        className='border-[2px] border-tp-neutral bg-tp-neutral py-4 px-4 rounded-2xl font-eucliarMedium xs:text-4 md:text-6 xs:leading-5 md:leading-7 focus:border-tp-neutral focus:ring-0 '
         {...rest}
       >
         {map(options, (val) => (
@@ -14,7 +14,7 @@ const Select = ({ label, options, className, ...rest }) => {
         ))}
       </select>
       {rest.hint && (
-        <div className='relative'><p className='md:absolute text-13 font-medium italic text-tp-purple'>{rest.hint}</p></div>
+        <div className=''><p className='text-13 font-normal text-tp-gray-200'>{rest.hint}</p></div>
       )}
     </div>
   );
--- src/elements/Form/Textfield.jsx
+++ src/elements/Form/Textfield.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Textfield = ({ label, ...rest }) => {
   return (
     <div className='flex flex-col xs:mb-5 md:mb-11 xs:w-full md:w-auto md:max-w-[500px]'>
-      <label className='text-[13px] text-black font-medium uppercase'>{label}</label>
+      <label className='text-[13px] text-black font-eucliarMedium font-medium uppercase leading-8'>{label}</label>
       <input
-        className='border-[2px] border-tp-gray-300 py-4 px-4'
+        className={`border-[2px] border-tp-gray-300 py-4 px-4 ${rest.className || ''}`}
         type={rest.type || "text"}
         placeholder={rest.placeholder || label}
         {...rest}
