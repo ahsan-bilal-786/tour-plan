195b510b7cdfe1cf31dbcd1a74ffb265e0b79dfa
Ahsan Bilal <engr.ahsan.bilal@gmail.com>
2023-04-13 00:11:54 +0500

    request room ui implementation done (#14)

--- src/components/LightningGenie/UnofficialTips.jsx
+++ src/components/LightningGenie/UnofficialTips.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import UnofficialTipCard from 'components/LightningGenie/UnofficialTipCard';
 
-const UnofficialTip = () => {
+const UnofficialTip = ({bg} ) => {
   return (
-    <section className="bg-tp-purple-500 text-white">
-      <div className="container mx-auto bg-primary text-center py-67">
+    <section className= {bg || "bg-tp-purple-500"} >
+      <div className="container mx-auto bg-primary text-center py-67 text-white">
         <h1 className="text-30 mb-56 font-groteskSemiBold text-tp-purple font-semibold">Unofficial Tips to Maximize Your Time</h1>
         <div className="flex xs:flex-col md:flex-row gap-x-3">
-          <UnofficialTipCard />
+          <UnofficialTipCard  />
           <UnofficialTipCard />
           <UnofficialTipCard />
           <UnofficialTipCard />
