7b1b83f7accb225b06bb791c1507166df4d9311b
ahsan-bilal-786 <engr.ahsan.bilal@gmail.com>
2023-07-06 18:13:31 +0500

    Fixed form issues

--- src/components/CustomDateParkMeals/Form.jsx
+++ src/components/CustomDateParkMeals/Form.jsx
@@ -8,28 +8,28 @@ import Button from "elements/Button";
 
 const Form = () => (
   <>
-    <div className="flex flex-col gap-y-12 w-3/4">
-      <div className="flex flex-row">
-        <div className="flex-1">
+    <div className="flex flex-col gap-y-12 xs:w-full md:w-3/4">
+      <div className="flex flex-row xs:justify-between">
+        <div className="md:flex-1">
           <TimeField title="WHAT TIME OF DAY?" />
         </div>
-        <div className="flex-1">
+        <div className="md:flex-1">
           <Dropdown
             title="FLEXIBILITY"
             options={["10 minutes", "09 minutes", "08 minutes", "07 minutes"]}
-            dropdownWrapper="min-w-[227px]"
+            dropdownWrapper="md:min-w-[227px]"
           />
         </div>
       </div>
-      <div className="flex flex-row">
-        <div className="flex-1">
+      <div className="flex flex-row xs:justify-between xs:gap-x-4 md:gap-0 ">
+        <div className="xs:w-1/2 md:flex-1">
           <Dropdown
             title="HOW LONG DO YOU PLAN TO BE THERE?"
             options={["10 minutes", "09 minutes", "08 minutes", "07 minutes"]}
-            dropdownWrapper="min-w-[227px]"
+            dropdownWrapper="md:min-w-[227px]"
           />
         </div>
-        <div className="flex-1">
+        <div className="xs:w-1/2 md:flex-1">
           <Textfield title="CONFIRMATION NUMBER (IF APPLICABLE)" />
         </div>
       </div>
